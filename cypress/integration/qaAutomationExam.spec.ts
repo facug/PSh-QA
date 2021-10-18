22c02f28d2082f2c23e8619434a7d41816ad1b05
unknown <facundogmonzon@gmail.com>
2021-10-18 18:58:00 -0300

    adding first spec

---
+++ cypress/integration/qaAutomationExam.spec.ts
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+describe("it will do an exam", function() {
+    it("Access Our Client list and prints all clients and how many they are", function() {
+
+    });
+    it("Navigate to blog and prints first tweet found", function() {
+
+    });
+    it("Prints all social media urls in footer section", function() {
+
+    });
+    it("Takes screenshot from the homepage", function() {
+
+    });
+});
